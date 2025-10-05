1ab432da991a7bece796892363baa43aedd64c21
jsstoni <jsstoniha@gmail.com>
2025-10-05 13:34:18 -0300

    add shadcn and dark mode ui

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
+import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://jsstoni.github.io',
+
   vite: {
     plugins: [tailwindcss()],
   },
+
+  integrations: [react()],
 });
